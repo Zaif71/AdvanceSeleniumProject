9a6b207db3577362956603e4929137fb1df82bd7
Zaffar <zaffar4534@gmail.com>
2026-01-13 16:18:37 +0530

    Enable headless Chrome for CI

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -3,8 +3,7 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -14,44 +13,34 @@ public class DriverFactory {
 
     public static void initDriver() {
 
-        // Read browser from System property, default = chrome
         String browser = System.getProperty("browser", "chrome");
+        boolean isCI = System.getenv("CI") != null;
 
-        switch (browser.toLowerCase()) {
+        if (browser.equalsIgnoreCase("chrome")) {
 
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver.set(new ChromeDriver());
-                break;
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
 
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver.set(new FirefoxDriver());
-                break;
+            if (isCI) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
 
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver.set(new EdgeDriver());
-                break;
-
-            default:
-                throw new RuntimeException("Unsupported browser: " + browser);
+            driver.set(new ChromeDriver(options));
         }
 
-        // Common browser setup
-        getDriver().manage().window().maximize();
         getDriver().manage().timeouts()
-                .implicitlyWait(Duration.ofSeconds(
-                        Integer.parseInt(ConfigReader.get("implicitWait"))
-                ));
+                .implicitlyWait(Duration.ofSeconds(10));
+        getDriver().manage().window().maximize();
     }
 
-    // Thread-safe driver getter
     public static WebDriver getDriver() {
         return driver.get();
     }
 
-    // Clean up driver after test
     public static void quitDriver() {
         if (driver.get() != null) {
             driver.get().quit();
